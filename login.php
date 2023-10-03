a28a1bc2c18f68745392c6eab7f29bd3ccddeee7
PhanLanNhi <phanlannhi777@gmail.com>
2023-10-04 00:26:36 +0700

    C1: add file

--- login.php
+++ login.php
@@ -11,39 +11,9 @@
 </head>
 <body>
     <div class="container-fluid p-0">
-        <!-- header -->
-        <div class="header mb-4 p-3">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="header__left d-flex">
-                        <div class="header__img" style="width: 50%;">
-                            <img src="./asset/img/logo.png" alt="" style="width: 80%;">
-                        </div>
-                        <nav class="navbar navbar-expand-lg">
-                            <div class="collapse navbar-collapse" id="navbarNav">
-                                <ul class="navbar-nav">
-                                  <li class="nav-item">
-                                    <a class="nav-link" href="#">Trang chủ</a>
-                                  </li>
-                                  <li class="nav-item">
-                                    <a class="nav-link" href="#">Đăng nhập</a>
-                                  </li>
-                                </ul>
-                            </div>
-                        </nav>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="header__right d-flex justify-content-end mt-4 me-3">
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Nội dung cần tìm" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Tìm</button>
-                          </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+        <?php
+            include 'header.php';
+        ?>
         <!-- main -->
         <div class="main">
             <div class="row">
